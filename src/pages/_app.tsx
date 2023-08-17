04388f6d1448bbdc11be4feb938186334b908a4b
Nikolin Andoni <97979235+N1k0l1n@users.noreply.github.com>
2023-08-17 13:52:38 +0200

    add paging

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Anton } from "next/font/google";
+import { Kreon } from "next/font/google";
 import { Container } from "react-bootstrap";
 import Head from 'next/head'
 
 
-const anton = Anton({ subsets: ["latin"], weight: ["400"] });
+const anton = Kreon({ subsets: ["latin"], weight: ["400"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
